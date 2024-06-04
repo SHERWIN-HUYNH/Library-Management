b90ed63b0b1a63f8e193e03ceeb16771b5e3eb3b
duyen3923 <riel3923@gmail.com>
2024-06-04 16:12:00 +0700

    Update HomeController.java

--- QLTV/src/main/java/Model/Controller/HomeController.java
+++ QLTV/src/main/java/Model/Controller/HomeController.java
@@ -22,14 +22,6 @@ public class HomeController extends BaseController {
 		_mvShare.setViewName("user/index");
 		return _mvShare;
 	}
-
-	@RequestMapping(value = "/notification")
-	public ModelAndView ThongBao() {
-		ModelAndView mv = new ModelAndView("user/notification");
-		mv.addObject("notifications", _HomeService.GetDataNotification());
-		return mv;
-	}
-
 	@RequestMapping(value = "/bookDetail/{id}")
 	public ModelAndView BookDetail(@PathVariable int id ) {
 		ModelAndView mv = new ModelAndView("user/book_detail");
