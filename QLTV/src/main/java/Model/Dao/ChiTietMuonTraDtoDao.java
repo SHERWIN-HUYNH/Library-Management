122f3d3a49318bcac54f47e733f83dc2acb43eb4
duyen3923 <riel3923@gmail.com>
2024-06-15 23:08:08 +0700

    tối ưu truy vấn trang ChiTietMuonTraDtoDao,BookDtoDao,TraSachDao

--- QLTV/src/main/java/Model/Dao/ChiTietMuonTraDtoDao.java
+++ QLTV/src/main/java/Model/Dao/ChiTietMuonTraDtoDao.java
@@ -18,22 +18,20 @@ public class ChiTietMuonTraDtoDao {
 
 	public List<ChiTietMuonTraDto> GetDataChiTietMuonTraDto() {
 		List<ChiTietMuonTraDto> list = new ArrayList<ChiTietMuonTraDto>();
-		String sql = "SELECT chitietmuontra.id as ctmtId, chitietmuontra.bookId, chitietmuontra.ngayMuon as ctmtNgayMuon,"
-				+ "chitietmuontra.trangThai as ctmtTrangThai,chitietmuontra.ngayTra as ctmtNgayTra,chitietmuontra.amount as ctmtAmount,"
-				+ "book.name as bookName,book.amount as bookAmount,reader.id as readerId,reader.name as readerName"
-				+ " FROM chitietmuontra, book, reader"
-				+ " WHERE chitietmuontra.bookId = book.id and chitietmuontra.readerId  = reader.id";
+		String sql = "SELECT " + "ctmt.id AS ctmtId," + "ctmt.bookId," + "ctmt.ngayMuon AS ctmtNgayMuon," + "ctmt.trangThai AS ctmtTrangThai," + 
+				"ctmt.ngayTra AS ctmtNgayTra," + "ctmt.amount AS ctmtAmount," + "b.name AS bookName," + "b.amount AS bookAmount," + 
+				"r.id AS readerId," + "r.name AS readerName" + "FROM " + "chitietmuontra ctmt" + "JOIN " + "book b ON ctmt.bookId = b.id" + 
+				"JOIN " + "reader r ON ctmt.readerId = r.id";
 		list = _jdbcTemplate.query(sql, new ChiTietMuonTraDtoMapper());
 		return list;
 	}
 
 	public List<ChiTietMuonTraDto> GetDataSearchChiTietMuonTraDto(String name) {
 		List<ChiTietMuonTraDto> list = new ArrayList<ChiTietMuonTraDto>();
-		String sql = "SELECT " + "cmt.id AS ctmtId," + "cmt.bookId," + "cmt.ngayMuon AS ctmtNgayMuon,"
-				+ "cmt.trangThai AS ctmtTrangThai," + "cmt.ngayTra AS ctmtNgayTra," + "cmt.amount AS ctmtAmount,"
-				+ "b.name AS bookName," + "b.amount AS bookAmount," + "r.id AS readerId," + "r.name AS readerName "
-				+ "FROM " + "chitietmuontra cmt " + "JOIN " + "book b ON cmt.bookId = b.id " + "JOIN "
-				+ "reader r ON cmt.readerId = r.id " + "WHERE " + "r.name LIKE ?";
+		String sql = "SELECT " + "cmt.id AS ctmtId, " + "cmt.bookId, " + "cmt.ngayMuon AS ctmtNgayMuon, " + "cmt.trangThai AS ctmtTrangThai, " +
+	             "cmt.ngayTra AS ctmtNgayTra, " + "cmt.amount AS ctmtAmount, " + "b.name AS bookName, " + "b.amount AS bookAmount, " +
+	             "r.id AS readerId, " + "r.name AS readerName " + "FROM " + "chitietmuontra cmt " + "JOIN " +"book b ON cmt.bookId = b.id " +
+	             "JOIN " + "reader r ON cmt.readerId = r.id " + "WHERE " + "r.name LIKE ?";
 		list = _jdbcTemplate.query(sql, new ChiTietMuonTraDtoMapper(), "%" + name + "%");
 		return list;
 	}
